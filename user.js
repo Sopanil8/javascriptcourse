855e6db01dbdff21497060d35d3460c9cdcc4d85
Sopanil8 <Sopaniljoshi8@gmail.com>
2024-12-09 17:58:55 +0545

    first commit

---
+++ user.js
@@ -0,0 +1,12 @@
+
+var name ='sopnil'
+var email='sopnol@gmail.com'
+var roll=10
+var phone=986798373
+var address="Baneshwor"
+
+document.getElementById("name").innerHTML=name
+document.getElementById("email").innerHTML=email
+document.getElementById("roll").innerHTML=roll
+document.getElementById("phone").innerHTML=phone
+document.getElementById("address").innerHTML=address
